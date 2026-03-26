23b581875b512e42b80b31a7bce2f20287620d00
boccad <diego.boccalari@sm-optics.com>
2026-03-26 21:57:13 +0100

    Upgrading Layout

--- html/moni.php
+++ html/moni.php
@@ -12,10 +12,11 @@
 <body>
   <img class="img-top" src="img/logo.png?random=323527528432525.24234" alt="">
   <h2><?php echo REPORT_NAME;?></h2>
-  <div><?php include_once 'buttons.php';?></div>
+  <nav class="nav-buttons">
+    <?php include_once 'buttons.php';?>
+  </nav>
   <noscript>You must enable JavaScript</noscript>
   <p id="moni"></p>
-  <!-- LOG monitor -->
   <fieldset class="big">
     <legend><b>.: Call log window :.</b></legend>
     <pre id="log"></pre>
@@ -23,10 +24,8 @@
   <footer>
     <p>
       Copyright (c) 2016-<?php echo date("Y");?><br>
-      The Regents of the <a target="_blank" href=http://k0usy.mystrikingly.com >K0USY Group</a>. All rights reserved.<br>
-      <a title="FDMR Monitor OA4DOA v1.0.0" target="_blank" href=https://github.com/yuvelq/FDMR-Monitor.git>Version OA4DOA</a>
-      <!-- Credits: SP2ONG 2019-2022 -->
-      <!-- THIS COPYRIGHT NOTICE MUST BE DISPLAYED AS A CONDITION OF THE LICENCE GRANT FOR THIS SOFTWARE. ALL DERIVATEIVES WORKS MUST CARRY THIS NOTICE -->
+      The Regents of the <a target="_blank" href="http://k0usy.mystrikingly.com">K0USY Group</a>. All rights reserved.<br>
+      <a title="FDMR Monitor OA4DOA v1.0.0" target="_blank" href="https://github.com/yuvelq/FDMR-Monitor.git">Version OA4DOA</a>
     </p>
   </footer>
 </body>
